2f6318ffb21c73d9accaac764d907db88c32ad21
maddiodie <maddiodie@gmail.com>
2024-12-14 17:37:27 -0700

    finished implementing readme instructions, passing all tests

--- src/com/amazon/ata/dynamodbscanandserialization/paginatedScan/FoodItemDao.java
+++ src/com/amazon/ata/dynamodbscanandserialization/paginatedScan/FoodItemDao.java
@@ -1,9 +1,15 @@
 package com.amazon.ata.dynamodbscanandserialization.paginatedScan;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.ScanResultPage;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Provides access to the FoodItems table.
@@ -21,13 +27,30 @@ public class FoodItemDao {
     }
 
     /**
-     * Uses the scanPage() method to scan the FoodItems retrieving only the provided limit number of items.
+     * Uses the scanPage() method to scan the FoodItems retrieving only the provided limit number of
+     * items.
      * @param exclusiveStartKey the item to start the scan at
      * @param limit the upper limit of items scanned by the scan
      * @return the list of FoodItems that is returned from the database
      */
     public List<FoodItem> scanFoodItemsWithLimit(final FoodItem exclusiveStartKey, final int limit) {
-        //TODO: replace the below code
-        return Collections.emptyList();
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression().withLimit(limit);
+
+        if (exclusiveStartKey != null) {
+            Map<String, AttributeValue> startKeyMap = new HashMap<>();
+            startKeyMap.put("id", new AttributeValue().withS(exclusiveStartKey.getId()));
+            scanExpression.setExclusiveStartKey(startKeyMap);
+        }
+
+        ScanResultPage<FoodItem> foodItemPage = mapper.scanPage(FoodItem.class, scanExpression);
+
+        return foodItemPage.getResults();
     }
+
+    /*
+    You want to implement the method, scanFoodItemsWithLimit(), in the FoodItemDao class to scan the
+    table for all the food items, so you can allow the store manager to browse the inventory. You want
+    to limit the amount of items that you retrieve at one time since the table is very large.
+     */
+
 }
